b43d17bfd8daa5e9ea1afd5eb12b31d6f77575e7
JJ <gongjian_001@126.com>
2018-01-09 10:21:21 +0800

    up

---
+++ JJCommonTool/JJCommonTool.h
@@ -0,0 +1,19 @@
+//
+//  JJCommonTool.h
+//  JJCommonTool
+//
+//  Created by 宫健(金融壹账通客户端研发团队) on 09/01/2018.
+//  Copyright © 2018 宫健(金融壹账通客户端研发团队). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJCommonTool.
+FOUNDATION_EXPORT double JJCommonToolVersionNumber;
+
+//! Project version string for JJCommonTool.
+FOUNDATION_EXPORT const unsigned char JJCommonToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJCommonTool/PublicHeader.h>
+
+
